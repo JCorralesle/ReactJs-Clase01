e0d2da9bd877489a5cce65f887cfa0af64964d0e
jcorrales <jcorrales.ica@gmail.com>
2022-07-10 15:17:55 -0500

    Navbar

---
+++ Clase3/ternaryOperator.js
@@ -0,0 +1,12 @@
+const edad = 15
+const condition = edad > 18
+let result = null
+/*if(condition) {
+    result = "correcto"
+}
+else {
+    result = "Incorrecto"
+}
+console.log (`La Variable result es "${result}"`)*/
+result = condition ? 45 : 100
+console.log(result)
